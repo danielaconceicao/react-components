13c218d0d0091a23ed710b6cb0f8bb8787d25649
danielaconceicao <daniela.3597@gmail.com>
2024-11-12 17:01:14 +0100

    separating the styles and using css module for the cards

--- src/components/CardsBlog/cards.jsx
+++ src/components/CardsBlog/cards.jsx
@@ -1,3 +1,18 @@
+import style from './cards.module.css'
+
 export default function Cards(){
-    
+    return (
+        <div className={style.flex}>
+            <div className={style.top}>
+                <p>600 x 400</p>
+            </div>
+
+            <div className={style.buttom}>
+                <h3>Titolo del Post</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius eum ea minus exercitationem dolores magni, autem dolore quam vero inventore quos nemo beatae esse animi, earum neque quod? Error, iusto.</p>
+                <button>LEGGI DI PIU</button>
+            </div>
+        
+        </div>
+    );
 }
